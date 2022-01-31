e534614740c441e0ea19492b713cd900eeb6060b
Regan <52940978+reganjs@users.noreply.github.com>
2022-01-31 11:13:14 -0700

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,3 +1,5 @@
+//This code was provided to me and simply needed debugging for a module in my LHL program. The code needed to be reformatted and had a typo. 
+
 var input = process.argv[2];
 
 if (input)
