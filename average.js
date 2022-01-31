0efcfdf0ca7c0ee3bb4297a4350fa33b1295c905
Regan <52940978+reganjs@users.noreply.github.com>
2022-01-31 11:14:27 -0700

    Update average.js

--- average.js
+++ average.js
@@ -1,3 +1,6 @@
+//This code was provided to me for my LHL program. It was part of a debugging module. The code needed to be reformatted and was missing the all important '}'. A '+' character also
+//needed to be removed. 
+
 function average(list) {
     var sum = 0;
 
