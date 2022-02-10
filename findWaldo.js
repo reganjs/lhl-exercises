d42104b4e3deac30351d38c918fc4d4918d024e9
Regan <52940978+reganjs@users.noreply.github.com>
2022-02-10 11:49:21 -0700

    Add files via upload

---
+++ findWaldo.js
@@ -0,0 +1,34 @@
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = (names, found) => {
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    if (name === "Waldo") {
+      let index = i;
+      found(index); // execute callback
+    }
+  }
+}
+
+const actionWhenFound = function (index) {
+  console.log(`Found at index ${index}`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+//refactored findWaldo function using a for each loop instead
+
+/* function findWaldo(array, found) {
+  array.forEach(function (item, index, _array) {
+    if (item === "Waldo") {
+      found(index);
+
+    }
+  });
+}
+
+const actionWhenFound = function (index) {
+  console.log(`Found at index ${index}`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound); */
